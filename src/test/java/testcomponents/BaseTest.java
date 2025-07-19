295d681c55edfdfc422e40c2050db0a1f59e31d1
kunalchavan <kunal24chavan@gmail.com>
2025-07-19 12:10:35 +0530

    TestNg Listeners

--- src/test/java/testcomponents/BaseTest.java
+++ src/test/java/testcomponents/BaseTest.java
@@ -34,7 +34,7 @@ public class BaseTest {
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/GlobalData.properties");
         prop.load(fis);
-        String browserName = prop.getProperty("browser");
+        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
         if (browserName.equalsIgnoreCase("Chrome")) {
             System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/SeleniumDrivers/chromedriver");
             driver = new ChromeDriver();
